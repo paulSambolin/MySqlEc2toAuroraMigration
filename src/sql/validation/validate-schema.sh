a00f47eadd8309185f82b3d81b256186fb35fbb2
jdenue <jamesdenue@JDENUE-MBP-NY.local>
2017-08-01 17:34:54 -0400

    adding flag to schema validation, fixing bug with data validation

--- src/sql/validation/validate-schema.sh
+++ src/sql/validation/validate-schema.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# look for flags passed
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -v|--verbose)
+    VERBOSE_FLAG="-vv"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift
+done
+
 # Import configuration parameters
 
 source config.cfg
@@ -17,8 +32,6 @@ read AURORA_PASSWORD
 stty echo
 printf "\n"
 
-
-
 # Run schema compare tool
 
-mysqldbcompare --server1=$EC2_MYSQL_USER_NAME:$EC2_MYSQL_PASSWORD@$EC2_MYSQL_HOST --server2=$AURORA_USER_NAME:$AURORA_PASSWORD@$AURORA_HOST $EC2_MYSQL_DATABASE:$AURORA_DATABASE --skip-checksum-table --skip-data-check --skip-row-count –vv
+echo mysqldbcompare --server1=$EC2_MYSQL_USER_NAME:$EC2_MYSQL_PASSWORD@$EC2_MYSQL_HOST --server2=$AURORA_USER_NAME:$AURORA_PASSWORD@$AURORA_HOST $EC2_MYSQL_DATABASE:$AURORA_DATABASE --skip-checksum-table --skip-data-check --skip-row-count $VERBOSE_FLAG
